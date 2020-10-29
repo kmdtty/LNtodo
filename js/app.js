601717b2ebdc12dce1c4334cc4c61636f0cc5144
Tatuya Kamada <kmdtty@users.noreply.github.com>
2020-10-29 17:29:51 +0900

    Store input item into the model

--- js/app.js
+++ js/app.js
@@ -1,84 +1,113 @@
 (function (window, document, rJS, jIO) {
-    var model = {
-      init: function() {
-        var storage = jIO.createJIO({"type": "indexeddb", "database": "foo"});
-        console.log(storage);
-        console.dir(storage.allDocs());
-        console.log('alldocs:' + storage.allDocs());
-        function foo(message) {
-          return storage.put("start", message)
-          .push(function (result) {
-              console.log('1st:' + result);
-              return storage.get("start");
-          })
-          .push(function (result) {
-              console.log('storage.get("start"):' + result);
-          }, function (error) {
-              console.warn(error);
-              throw error;
-          });
-        }
-        return foo("hello");
-      },
-    }
-
-    var octopus = {
-      init: function() {
-         model.init();
-      }
-    }
-    //octopus.init();
-    rJS(window)
-    .setState({item_list: [], current_item: null})
-    .declareService(function () {
-      var model_gadget;
-      return this.getDeclaredGadget("model")
-        .push(function (subgadget) {
-          console.log("model gadget as sub gadget");
-          model_gadget = subgadget;
-          return model_gadget.put("/", {title: "Test", completed: false });
-         });
-    })
-    .onStateChange(function (modification_dict) {
-      console.log("onStageChange is triggered");
-      if (modification_dict.hasOwnProperty("current_item")) {
-        this.addItem(modification_dict.current_item);
-        console.dir(modification_dict);
-        // what is this doing?? update the model?
-        return this.getDeclaredGadget("model")
-        .push(function (model_gadget) {
-          // why length is passed?
-          // => Since the index of item_list is unique, can be used as ID
-          return model_gadget.put(gadget.state.item_list.length.toString(), {
-            title: modification_dict.current_item,
-            completed: false
-          });
+  var model = {
+    init: function() {
+      var storage = jIO.createJIO({"type": "indexeddb", "database": "foo"});
+      console.log(storage);
+      console.dir(storage.allDocs());
+      console.log('alldocs:' + storage.allDocs());
+      function foo(message) {
+        return storage.put("start", message)
+        .push(function (result) {
+            console.log('1st:' + result);
+            return storage.get("start");
+        })
+        .push(function (result) {
+            console.log('storage.get("start"):' + result);
+        }, function (error) {
+            console.warn(error);
+            throw error;
         });
       }
-    })
-    .declareMethod("addItem", function (item) {
-      var list_item = document.createElement("LI");
-      list_item.appendChild(document.createTextNode(item));
-      this.element.querySelector("ul").appendChild(list_item);
-    })
-    .onEvent("submit", function (event) {
-      // what is event? form? input?
-      // what is index 0 ?
-      console.log("submit event is triggered");
-      // this gadget variable is needed for changeState
-      var gadget = this;
-      var item = event.target.elements[0].value;
-      event.target.elements[0].value = "";
-      // no need to return this??
-      return new RSVP.Queue()
+      return foo("hello");
+    },
+  }
+
+  var octopus = {
+    init: function() {
+       model.init();
+    }
+  }
+  //octopus.init();
+  rJS(window)
+  .setState({item_list: [], current_item: null})
+  .declareService(function () {
+    var model_gadget;
+    var gadget = this;
+    return this.getDeclaredGadget("model")
+      .push(function (subgadget) {
+        console.log("model gadget as sub gadget");
+        model_gadget = subgadget;
+        return model_gadget.put("/", {title: "Test", completed: false });
+       })
       .push(function () {
-        console.log('chaning state');
-        gadget.changeState({current_item: item});
+        return model_gadget.allDocs();
+      })
+      .push(function (result_list) {
+        var promise_list = [];
+        // result_list is a Promise Queue which has data property
+        result_list.data.rows.map(function (row) {
+          // model_gadget.get returns a promise??
+          console.log('Test model_gadget.get');
+          console.dir(result_list);
+          console.log('row.id:' + row.id);
+          console.log(model_gadget.get(row.id));
+          // This get() is jIO.get so returns a Promise
+          promise_list.push(model_gadget.get(row.id));
+        });
+        return RSVP.all(promise_list);
+      })
+      .push(function (result_list) {
+        result_list.map(function (item) {
+          console.log('item.title:' + item.title);
+          gadget.addItem(item.title);
+        });
       });
-      // what is false, true here??
-      // => useCapture, and preventDefault
-      // the same with addEventListner()
-    }, false, true);
+  })
+  .onStateChange(function (modification_dict) {
+    console.log("onStageChange is triggered");
+    var gadget = this;
+    if (modification_dict.hasOwnProperty("current_item")) {
+      this.addItem(modification_dict.current_item);
+      // what is this doing?? update the model?
+      return this.getDeclaredGadget("model")
+      .push(function (model_gadget) {
+        console.log('updating model');
+        // why length is passed?
+        // => Since the index of item_list is unique, can be used as ID
+        console.log(gadget.state.item_list.length.toString());
+        return model_gadget.put(gadget.state.item_list.length.toString(), {
+          title: modification_dict.current_item,
+          completed: false
+        });
+      });
+    }
+  })
+  .declareMethod("addItem", function (item) {
+    var list_item = document.createElement("LI");
+    list_item.appendChild(document.createTextNode(item));
+    this.element.querySelector("ul").appendChild(list_item);
+    // Can we directory push item here??
+    this.state.item_list.push(item);
+  })
+  .onEvent("submit", function (event) {
+    // what is event? form? input?
+    // what is index 0 ?
+    console.log("submit event is triggered");
+    // this gadget variable is needed for changeState
+    var gadget = this;
+    var item = event.target.elements[0].value;
+    event.target.elements[0].value = "";
+    // no need to return this??
+    return new RSVP.Queue()
+    .push(function () {
+      console.log('chaning state');
+      // we do not need to push item_list here??
+      gadget.changeState({current_item: item});
+    });
+    // what is false, true here??
+    // => useCapture, and preventDefault
+    // the same with addEventListner()
+  }, false, true);
 
 }(window, document, rJS, jIO));
 
