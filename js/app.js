dfab170826a6d045a801993c30732d88f8db3eb2
Tatuya Kamada <kmdtty@users.noreply.github.com>
2020-10-30 12:06:12 +0900

    Remove some logging, add some comment

--- js/app.js
+++ js/app.js
@@ -6,12 +6,8 @@
     var model_gadget;
     var gadget = this;
     return this.getDeclaredGadget("model")
-      .push(function (subgadget) {
-        console.log("model gadget as sub gadget");
-        model_gadget = subgadget;
-        return model_gadget.put("/", {title: "Test", completed: false });
-       })
-      .push(function () {
+      .push(function (gadget) {
+        model_gadget = gadget;
         return model_gadget.allDocs();
       })
       .push(function (result_list) {
@@ -21,7 +17,6 @@
         // It seems almost useless here.
         result_list.data.rows.map(function (row) {
           // model_gadget.get returns a promise??
-          //console.log(model_gadget.get(row.id));
           // This get() is jIO.get so returns a Promise
           promise_list.push(model_gadget.get(row.id));
         });
@@ -32,8 +27,6 @@
         console.log('result_list:')
         console.dir(result_list)
         result_list.map(function (item) {
-          console.log('item.title:' + item.title);
-          //gadget.addItem(item.title);
           // directory put into state???
           gadget.state.item_list.push(item.title);
         });
@@ -42,10 +35,7 @@
   })
   .onStateChange(function (modification_dict) {
     // what should we do in onStateChage???
-    console.log("onStageChange is triggered");
     var gadget = this;
-    console.log('modification_dict:');
-    console.log(modification_dict);
     this.element.querySelector("ul").innerHTML =
       "<li>" + this.state.item_list.join("</li>\n<li>") + "</li>";
     // We can not changeState({update: false}) here.
@@ -57,6 +47,7 @@
     // Can we directory push item here??
     // Some properties are updated through changeState() and item_list are
     // directory write into state ???
+    // If so, what for change state? just notify the modification to render??
     gadget.state.item_list.push(item);
     return gadget.getDeclaredGadget("model")
       .push(function (model_gadget) {
